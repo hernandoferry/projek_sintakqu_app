53d3f10ee4b255ba1d8c6eb473e5d1d0259ed875
hernando_ferry <hernando.ferry@gmail.com>
2026-07-03 15:37:29 +0700

    change: setting beberapa file untuk keperluan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:sintakqu/database/db_helper.dart';
+import 'package:sintakqu/firebase_options.dart';
 import 'package:sintakqu/login.dart';
 import 'package:sintakqu/splash_screen.dart';
 import 'package:sintakqu/view/home/index_home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
