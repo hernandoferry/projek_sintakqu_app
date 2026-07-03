e88a8ad06d3e11c73c954f35fc4fd95ffa91dcce
hernando_ferry <hernando.ferry@gmail.com>
2026-07-03 11:08:02 +0700

    change : versi apk di pubspeck

---
+++ lib/services/dio_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+Dio createDioClient() {
+  final dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://jsonplaceholder.typicode.com',
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ),
+  );
+
+  dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+  return dio;
+}
